d0c26c7b6d87619266e086a9baed2bb1c32cac73
Josh Kralewski <joshkmanj@gmail.com>
2022-02-26 21:25:59 -0600

    Home page created and linked to next page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,25 @@
 // import { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-function Home(){
 
-    return(
-        <>
+function Home() {
 
-        </>
+    const history = useHistory();
+
+    const clickSubmit = ()=>{
+        history.push('/Feeling');
+    }
+
+    return (
+        <div className="body">
+            <div className="card">
+                <h2>Please fill out this survey!</h2>
+                <button
+                    className="next"
+                    onClick={clickSubmit}>Proceed</button>
+            </div>
+        </div>
     )
 }
 export default Home;
