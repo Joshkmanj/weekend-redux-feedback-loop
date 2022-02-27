8777e233f5f4ec9e04c93c913f2298c721db744b
Josh Kralewski <joshkmanj@gmail.com>
2022-02-27 16:01:20 -0600

    Review page is appending data to DOM

--- src/components/Pages/Review.jsx
+++ src/components/Pages/Review.jsx
@@ -1,11 +1,28 @@
 // import { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Review(){
 
+    const formData = useSelector(store => store.currentRatings)
+
+
+
     return(
         <>
-
+        <div className="body">
+            <div className="card">
+                <h3>Review before submitting!</h3>
+                <h4 className="faded-text">Does everything look accurate?</h4>
+                <h4>{formData.feelsRating}</h4>
+                <h4>{formData.understanding}</h4>
+                <h4>{formData.support}</h4>
+                <h4>{formData.comment}</h4>
+                <button
+                    className="next"
+                    // onClick={clickSubmit}
+                    >Next</button>
+            </div>
+        </div>
         </>
     )
 }
