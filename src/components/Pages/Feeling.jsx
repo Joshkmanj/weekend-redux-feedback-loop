9572e19981d6a67814e2d343af8bf19012c9f002
Josh Kralewski <joshkmanj@gmail.com>
2022-02-26 18:31:03 -0600

    Page rendering and capturing user's range data properly

--- src/components/Pages/Feeling.jsx
+++ src/components/Pages/Feeling.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 
-function Feeling(){
-    
+function Feeling() {
 
-    return(
-        <div>
-            <h3>How are you feeling today?</h3>
-            <textarea rows={4}  cols={60}/>
+    const [feelRating, setFeelRating] = useState(1)
+
+    const ratingHandler = (val) => {
+        console.log(val);
+        setFeelRating(val)
+    }
+
+    return (
+        <div className="body">
+            <div className="card">
+                <h3>How are you feeling today?</h3>
+                <span>Bad
+                    <input 
+                    className="rating" 
+                    value={feelRating}
+                    onChange={(event)=>{ratingHandler(event.target.value)}} 
+                    type="range" 
+                    min={1} max={5} />
+                    Great</span>
+            </div>
         </div>
     )
 }
